4283e18111f4313019123e850fa781cc0ac231d8
Edwin Brower <edwin.brower@gmail.com>
2017-06-28 17:05:23 -0700

    Update repolist to include entries

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import RepoListEntry from './RepoListEntry.jsx';
 
 const RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
+    {props.repos.map( repo => 
+      <RepoListEntry repo={repo} />
+    )}
   </div>
-)
+);
 
 export default RepoList;
